425bacd73bc0b512221c05ba4ff7c24f21a0018a
xiaxdaoZ <zz128144@gmail.com>
2017-04-23 16:39:32 +0800

    implement is_member_of?(gorup)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@ class User < ApplicationRecord
   has_many :groups
   has_many :posts
   has_many :group_relationship
-  has_many :participated_groups, :through => :group_relationship, :source => :group 
+  has_many :participated_groups, :through => :group_relationship, :source => :group
+
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
 end
